3903e38902acdcdc1b1320c2f9a1c68892814521
cforChobar <sidhuashman02@gmail.com>
2025-02-05 09:47:25 +0530

    hero section under contruction

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,14 @@
 const Hero = () => {
   return (
-    <div className='font-bold text-2xl font-body'>
-      I am hero section of the app let's design me
-    </div>
+    <section className="relative">
+      <div className="image-container w-full h-[98vh] relative">
+        <img src="/images/hero-image.jpg" alt="hero-section-image" className="w-full h-full rounded-2xl object-cover"/>
+        <div className="absolute w-full h-full bg-[linear-gradient(126deg,_rgba(8,8,8,0)_80%,_rgba(44,39,39,.6)_100%)] top-0 rounded-2xl"></div>
+      </div>
+      <div className="button absolute right-10 top-10">
+        <button className="font-semibold px-3 py-1 rounded-lg bg-background hover:bg-background-shade cursor-pointer">CONTACT</button>
+      </div>
+    </section>
   )
 }
 
